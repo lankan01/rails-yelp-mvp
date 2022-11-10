a48b39b0b446d7b5f80631bcd1d8a672dd731175
Jega Pradeeba <pjega92@gmail.com>
2022-11-10 15:29:17 +0800

    yelp mvp with create and see reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:index, :create]
+    resources :reviews, only: [:index, :new, :create]
   end
 end
